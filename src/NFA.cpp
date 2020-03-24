8461970a9d26f3109b2bcab9e5aeef1539d71aef
ziadTaha <ziadtaha197@gmail.com>
2020-03-25 01:29:33 +0200

    isa 5er

--- src/NFA.cpp
+++ src/NFA.cpp
@@ -54,24 +54,61 @@ void NFA::setToken( Token &token) {
     NFA::token = token;
 }
 
-NFA NFA::ast(NFA a1, Token e) {
-    //todo
-    return NFA(0, Token());
+ NFA NFA::ast(NFA a1,Token token) {
+
+    NFA nfa =NFA(0,token);
+
+    State strt =State(token);
+    State end=State(Token());
+
+    strt.setTransion(0,end);
+    end.setEndState(true);
+    nfa.setStartState(strt);
+
+    State a1start =a1.getStartState;
+    State a1end =a1.getEndState;
+    a1end.setTransion(0,end);
+    a1end.setTransion(0,a1start);
+    a1end.setEndState(false);
+    return nfa ;
 }
 
-NFA NFA::plusNFA(NFA a1, Token e) {
-    //todo
-    return NFA(0, Token());
+NFA NFA::plus(NFA a1,Token token) {
+    return concat(a1,ast(a1,token),token);
 }
 
-NFA NFA::concat(NFA a1, NFA a2 , Token e) {
-    //todo
-    return NFA(0, Token());
+NFA NFA::concat(NFA a1, NFA a2,Token token) {
+
+    NFA nfa =NFA(0,token);
+    nfa.setStartState(a1.getStartState);
+    /**
+     *  predicted pointers problem
+     * */
+    State a1end=a1.getEndState;
+    a1end.setEndState(false);
+    a1end.setTransion(0,a2.getStartState);
+    nfa.setEndState(a2.getEndState);
+
+    return nfa;
 }
 
-NFA NFA::oring(NFA a1, NFA a2 , Token e) {
-    //todo
-    return NFA(0, Token());
+NFA NFA::oring(NFA a1, NFA a2,Token token) {
+    NFA nfa =NFA(0,token);
+
+    State strt =State(token);
+    State end=State(token);
+    strt.setTransion(0,a1.getStartState);
+    strt.setTransion(0,a2.getStartState);
+
+    State a1end=a1.getEndState;
+    State a2end=a2.getEndState;
+    a1end.setTransion(0,end);
+    a2end.setTransion(0,end);
+
+    end.setEndState(true);
+    nfa.setStartState(strt);
+    nfa.setEndState(end);
+    return nfa;
 }
 
  vector <string> NFA::parcingPattern () {
@@ -81,12 +118,9 @@ NFA NFA::oring(NFA a1, NFA a2 , Token e) {
      string ns = "" ;
      for (int i = 0 ; i < s.size() ; i++) {
         if (s[i] == '|'){
-             arg.push_back(ns);
-             ns = "" ;
+
         } else if (s[i] == '(') {
 
-        }else if (s[i] = ' ' ){
-            continue ;
         }
 
         ns += s[i] ;
@@ -94,3 +128,127 @@ NFA NFA::oring(NFA a1, NFA a2 , Token e) {
 
      return arg;
  }
+NFA NFA::basicOp(std ::string str,Token token)
+{
+     char c =str.at(0);
+     if(c=='{')
+     {
+         return setOP(str.substr(1,str.length-2),token);
+     }
+     else if(c=='[')
+     {
+         return intervalOP(str.substr(1,str.length-2),token);
+     }
+     else
+     {
+         return charOP(c,token);
+     }
+
+}
+NFA NFA::setOP(std :: string str,Token token)
+{
+  State strt (token);
+  NFA nfa (0,token);
+  nfa.setStartState(strt);
+  int i=0;
+  State end (token);
+  char c =str.at(0);
+  while (c!='}')
+  {
+      c =str.at(i);
+      if((c>='A'&&c<='Z')
+      ||(c>='a'&&c<='z'))
+      {
+        strt.setTransion(c,end);
+        i++;
+      }
+      else if(c==92)
+      {
+         i++;
+         c =str.at(i);
+         if(c=='*'||c=='+'||c=='?')
+         {
+             strt.setTransion(c,end);
+         }
+         i++;
+
+
+      }
+      else if(c==','||c==' ')
+      {
+          i++;
+      }
+
+  }
+  end.setEndState(true);
+  nfa.setEndState(end);
+}
+NFA NFA::intervalOP(std :: string str,Token token)
+{
+  State strt (token);
+  NFA nfa (0,token);
+  nfa.setStartState(strt);
+  int i=0;
+  State end (token);
+  end.setEndState(true);
+  nfa.setEndState(end);
+  char c ;
+  char b=0,e=0;
+  while(c!=']')
+  {
+    c =str.at(i);
+    if((c>='A'&&c<='Z')
+      ||(c>='a'&&c<='z'))
+      {
+          if(b==0)
+          {
+              b=c;
+          }
+          else
+          {
+              e=c;
+          }
+          i++;
+      }
+      else if(c==92)
+      {
+         i++;
+         c =str.at(i);
+         if(c=='*'||c=='+'||c=='?')
+         {
+          if(b==0)
+          {
+              b=c;
+          }
+          else
+          {
+              e=c;
+          }
+         }
+         i++;
+      }
+      else if(c=='-'||c==' ')
+      {
+          i++;
+      }
+  }
+  i=0;
+  c=b;
+  while(c>=b&&c<=e)
+  {
+     strt.setTransion(c,end);
+     c++;
+  }
+}
+NFA NFA::charOP(char c,Token token)
+{
+      State strt (token);
+      NFA nfa (0,token);
+      nfa.setStartState(strt);
+      State end (token);
+      strt.setTransion(c,end);
+      end.setEndState(true);
+      nfa.setEndState(end);
+
+
+}
